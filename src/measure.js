fa20cbeaff0fd531ee967a973dd8ee79075372a6
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2024-11-05 13:18:06 +0100

    1730809084789488454

--- src/measure.js
+++ src/measure.js
@@ -1,12 +1,12 @@
 // 
 // Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
 // https://kekse.biz/ https://github.com/kekse1/javascript/
-// v0.2.0
+// v0.2.1
 // 
 // Two functions to measure the throughput of your data, in
 // bytes per second (for milliseconds remove the multiplication
-// with 1000); and the ETA ('Estimated Time Of Arrival'), in
-// milliseconds (multiply with 1000 for seconds).
+// with 1000); and the ETA ('Estimated Time Of Arrival') with
+// '.remaining()', in milliseconds (multiply *1000 for seconds).
 // 
 
 //
@@ -18,7 +18,7 @@ measure.throughput = (_size, _time) => {
 	if(_size <= 0 || _time <= 0) return 0;
 	return Math.max(0, (_size / _time * 1000)); };
 
-measure.estimated = (_rest, _size, _time) => {
+measure.remaining = (_rest, _size, _time) => {
 	if(typeof _size === 'bigint') _size = Number(_size);
 	if(typeof _time === 'bigint') _time = Number(_time / 1000000n);
 	if(typeof _rest === 'bigint') _rest = Number(_rest);
